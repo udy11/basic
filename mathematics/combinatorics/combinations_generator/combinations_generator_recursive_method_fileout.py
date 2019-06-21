e8eab63cdc0c8cd62e040efaf943d22532c9f61b
udy11 <udaya_cbscients@yahoo.com>
2019-06-22 01:12:42 +0530

    Add files via upload

---
+++ mathematics/combinatorics/combinations_generator/combinations_generator_recursive_method_fileout.py
@@ -0,0 +1,59 @@
+# Last updated: 15-Aug-2013
+# Udaya Maurya (udaya_cbscients@yahoo.com)
+# Function to get combinations of objects
+
+# Method self-discovered, so there may be better ones
+
+# ALL YOU NEED TO DO:
+# Call cmbsf() function with a, r and ofln
+#   where a is list/tuple/array
+#   r is what combinations are required
+#   ofln is output file name (will be overwritten)
+
+# Method is to use successive calls of same function
+# to populate the list with combinations
+
+# Output is sorted according to indices of a
+# Same elements of a are treated as distinct
+
+def cmbsf(a, r, ofln):
+    ''' (list/tuple, int, str) -> list of list
+        Writes r combinations of list/tuple a to file ofln
+    '''
+    n = len(a)
+    if r < 1 or r > n:
+        return
+    ofl = open(ofln, 'w')
+    if r == 1:
+        for i in a:
+            ofl.write(str([i]) + '\n')
+        ofl.close()
+        return
+    ct = []
+    m = 0
+    i = 0
+    def cmbs():
+        nonlocal m, ct, i
+        if m == 0:
+            for k in range(n-r+2):
+                i = k
+                ct = [a[k]]
+                m = 1
+                cmbs()
+        elif m == r-1:
+            for k in range(i+1, n):
+                ofl.write(str(ct + [a[k]]) + '\n')
+        else:
+            for k in range(i+1, n):
+                i = k
+                ct.append(a[k])
+                m += 1
+                cmbs()
+                ct = ct[:-1]
+                m -= 1
+    cmbs()
+    ofl.close()
+
+ofln = 'combs.txt'
+a = (1, 2, 3, 4, 5, 6)
+cmbsf(a, 3, ofln)
---
+++ mathematics/combinatorics/combinations_generator/combinations_generator_recursive_method_listout.py
@@ -0,0 +1,58 @@
+# Last updated: 15-Aug-2013
+# Udaya Maurya (udaya_cbscients@yahoo.com)
+# Function to get combinations of objects
+
+# Method self-discovered, so there may be better ones
+
+# ALL YOU NEED TO DO:
+# Call cmbsl() function with a and r
+#   where a is list/tuple/array
+#   and r is what combinations are required
+
+# Method is to use successive calls of same function
+# to populate the list with combinations
+
+# Output is sorted according to indices of a
+# Same elements of a are treated as distinct
+
+def cmbsl(a, r):
+    ''' (list/tuple, int) -> list of list
+        Returns r combinations of list/tuple a
+
+        >>> a = [1, 2, 3, 4]; r = 3
+        >>> cmbsl(a, r)
+        [[1, 2, 3], [1, 2, 4], [1, 3, 4], [2, 3, 4]]
+    '''
+    if r == 1:
+        return [[i] for i in a]
+    n = len(a)
+    if r < 1 or r > n:
+        return []
+    ct = []
+    cm = []
+    m = 0
+    i = 0
+    def cmbs():
+        nonlocal m, ct, cm, i
+        if m == 0:
+            for k in range(n-r+2):
+                i = k
+                ct = [a[k]]
+                m = 1
+                cmbs()
+        elif m == r-1:
+            for k in range(i+1, n):
+                cm.append(ct + [a[k]])
+        else:
+            for k in range(i+1, n):
+                i = k
+                ct.append(a[k])
+                m += 1
+                cmbs()
+                ct = ct[:-1]
+                m -= 1
+    cmbs()
+    return cm
+
+a = (1, 2, 3, 4, 5, 6)
+print(cmbsl(a, 3))
