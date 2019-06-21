38fe2fab718ec9a8721e9a9ab53e34ddc7f580f1
udy11 <udaya_cbscients@yahoo.com>
2019-06-22 01:13:27 +0530

    Add files via upload

---
+++ mathematics/combinatorics/n-tuples_generator/n-tuples_generator_recursive_method_fileout.py
@@ -0,0 +1,57 @@
+# Last updated: 15-Aug-2013
+# Udaya Maurya (udaya_cbscients@yahoo.com)
+# Function to get n-tuples of objects
+
+# Method self-discovered, so there may be better ones
+
+# ALL YOU NEED TO DO:
+# Call ntpsf() function with a, r and ofln
+#   where a is list/tuple/array
+#   and r is what r-tuples are required
+#   ofln is output file name (will be overwritten)
+
+# Method is to use successive calls of same function
+# to populate the list with n-tuples
+
+# Output is sorted according to indices of a
+# Same elements of a are treated as distinct
+
+def ntpsf(a, r, ofln):
+    ''' (list/tuple, int, str) -> list of list
+        Writes r-tuples of list/tuple a to file ofln
+    '''
+    n = len(a)
+    if r < 1 or r > n:
+        return 0
+    ofl = open(ofln, 'w')
+    if r == 1:
+        for i in a:
+            ofl.write(str([i]) + '\n')
+        ofl.close()
+        return n
+    ct = []
+    m = 0
+    def ntps():
+        nonlocal m, ct
+        if m == 0:
+            for k in range(n):
+                ct = [a[k]]
+                m = 1
+                ntps()
+        elif m == r-1:
+            for k in range(n):
+                ofl.write(str(ct + [a[k]]) + '\n')
+        else:
+            for k in range(n):
+                ct.append(a[k])
+                m += 1
+                ntps()
+                ct = ct[:-1]
+                m -= 1
+    ntps()
+    ofl.close()
+    return n ** r
+
+ofln = 'ntpls.txt'
+a = (1, 2, 3, 4, 5)
+ntpsf(a, 3, ofln)
---
+++ mathematics/combinatorics/n-tuples_generator/n-tuples_generator_recursive_method_listout.py
@@ -0,0 +1,55 @@
+# Last updated: 15-Aug-2013
+# Udaya Maurya (udaya_cbscients@yahoo.com)
+# Function to get n-tuples of objects
+
+# Method self-discovered, so there may be better ones
+
+# ALL YOU NEED TO DO:
+# Call ntpsl() function with a and r
+#   where a is list/tuple/array
+#   and r is what r-tuples are required
+
+# Method is to use successive calls of same function
+# to populate the list with n-tuples
+
+# Output is sorted according to indices of a
+# Same elements of a are treated as distinct
+
+def ntpsl(a, r):
+    ''' (list/tuple, int) -> list of list
+        Returns r-tuples of list/tuple a
+
+        >>> a = [1, 2]; r = 2
+        >>> ntpsl(a, r)
+        [[1, 1], [1, 2], [2, 1], [2, 2]]
+    '''
+    if r == 1:
+        return [[i] for i in a]
+    n = len(a)
+    if r < 1 or r > n:
+        return []
+    ct = []
+    nt = []
+    m = 0
+    def ntps():
+        nonlocal m, ct, nt
+        if m == 0:
+            for k in range(n):
+                ct = [a[k]]
+                m = 1
+                ntps()
+        elif m == r-1:
+            for k in range(n):
+                nt.append(ct + [a[k]])
+        else:
+            for k in range(n):
+                ct.append(a[k])
+                m += 1
+                ntps()
+                ct = ct[:-1]
+                m -= 1
+    ntps()
+    return nt
+
+a = (1, 2, 3, 4, 5)
+print(ntpsl(a, 2))
