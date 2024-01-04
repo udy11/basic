fe494105ad8e6a10600c7eb37bb89683b8b7ecd5
Udaya Maurya <udaya_cbscients@yahoo.com>
2024-01-04 17:21:56 +0530

    -

--- misc/binary_search_nearest_number/bsnn.py
+++ misc/binary_search_nearest_number/bsnn.py
@@ -1,66 +1,49 @@
-# Last updated: 07-Jun-2013
+# Last updated: 04-Jan-2024
 # Udaya Maurya (udaya_cbscients@yahoo.com, telegram: https://t.me/udy11)
 # Source: https://github.com/udy11, https://gitlab.com/udy11
 # Function to binary search nearest number in a sorted array
 
 # ALL YOU NEED TO DO:
-# Call function bsa(a, n1, n2, b) with:
+# Call function binarySearchNN(a, x) with:
 #   a = numerical array in which nearest number to be searched
-#   n1 = first index from which to begin search (inclusive)
-#   n2 = last index upto which search is to be made (not inclusive)
-#   b = number whose nearest to be searched
-# NOTE that n2 is not included in the search
+#   x = number whose nearest index to be searched
 
 # OUTPUT:
 # Output of the functions is the index of number to be searched
 
-# For ascendingly sorted array
-def bsan_asc(a, n1, n2, b):
-    if b > a[n2 - 1]:
-        return n2 - 1
-    if b < a[n1]:
-        return n1
-    nd = n2 - n1
-    nm = n1 + nd // 2
-    while nd > 1:
-        if a[nm] > b:
-            n2 = nm
+# NOTE:
+# If x is at the center of two indices, output will be the left index
+
+def binarySearchNN(a, x):
+    ''' (array, num) -> int
+        Binary Search Nearest Number in a sorted Array of real numbers
+    '''
+    na = len(a)
+    if na < 2:
+        return 0
+    i0 = 0
+    i1 = na - 1
+    asc = -1 + 2 * (a[1] > a[0])    # 1 if ascending, -1 if descending
+    while i1 - i0 > 1:
+        im = (i0 + i1) // 2
+        dx = asc * (x - a[im])
+        if dx > 0:
+            i0 = im
+        elif dx < 0:
+            i1 = im
         else:
-            n1 = nm
-        nd = n2 - n1
-        nm = n1 + nd // 2
-    d1 = b - a[n1]
-    d2 = a[n2] - b
-    if d1 < d2:
-        return n1
+            return im
+    if asc * (a[i0] + a[i1] - 2*x) >= 0:
+        return i0
     else:
-        return n2
+        return i1
 
-# For descendingly sorted array
-def bsan_dec(a, n1, n2, b):
-    if b < a[n2 - 1]:
-        return n2 - 1
-    if b > a[n1]:
-        return n1
-    nd = n2 - n1
-    nm = n1 + nd // 2
-    while nd > 1:
-        if a[nm] < b:
-            n2 = nm
-        else:
-            n1 = nm
-        nd = n2 - n1
-        nm = n1 + nd // 2
-    d1 = a[n1] - b
-    d2 = b - a[n2]
-    if d1 < d2:
-        return n1
-    else:
-        return n2
-
-a = [11 + i * 0.1 for i in  range(1000)]
-b = 101.78
-
-print(bsan_asc(a, 0, 1000, b))
-a.reverse()
-print(bsan_dec(a, 0, 1000, b))
+if __name__ == '__main__':
+    a = [i*i for i in  range(1, 6)]
+    xx = (6.3, 6.5, 6.6, -1, 48, 1, 9, 25)
+    for _ in range(2):
+        print('Array:', a)
+        for x in xx:
+            print(f'Index nearest to {x} is', binarySearchNN(a, x))
+        print()
+        a.reverse()
